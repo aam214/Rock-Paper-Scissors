46d179e911a1b9cfbded82d976631d1f1dca66cb
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-08 23:39:25 -0500

    Added a meta description and title for SEO. Added the variable of play when the button is clicked. gameRound function added with if/else if/ else statements returning who won the game.

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
 button.addEventListener('click', () => {
-console.log("user clicked", button.id, 
-  "computer picked", computerChoice());
+ const play = gameRound(button.id, computerChoice());
+ console.log(play);
   });
 });
 
@@ -11,4 +11,18 @@ function computerChoice() {
 const choiceArray = ['rock', 'paper', 'scissors'];
 const randomChoice = Math.floor(Math.random() * choiceArray.length);
 return choiceArray[randomChoice];
+}
+
+function gameRound(userPick, computerPick) {
+if (userPick === computerPick) {
+return "It's a tie!";
+} else if (
+(userPick === 'rock' && computerPick === 'scissors') ||
+(userPick === 'scissors' && computerPick === 'paper') ||
+(userPick === 'paper' && computerPick === 'rock')
+){
+return "You win! " + userPick + " beats " + computerPick;
+} else{
+return "You lose! " + computerPick + " beats " + userPick;
+}
 }
