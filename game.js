f4b4ab620f565d229bfe8dbc4cebddb386f573c1
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-10 19:51:36 -0500

    Computer-score id added to HTML. Added elements of userScore and computerScore. Added increments for each player when one wins. Used textContent to show in the UI.

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 const buttons = document.querySelectorAll("button");
 const result = document.getElementById("result");
+const userScoreEl = document.getElementById("user-score");
+const computerScoreEl = document.getElementById("computer-score");
 
+let userScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) => {
 button.addEventListener('click', () => {
@@ -24,8 +28,12 @@ return "It's a tie!";
 (userPick === 'scissors' && computerPick === 'paper') ||
 (userPick === 'paper' && computerPick === 'rock')
 ){
+  userScore++;
+  userScoreEl.textContent = userScore;
 return "You won! " + userPick + " beats " + computerPick;
 } else{
+  computerScore++;
+  computerScoreEl.textContent = computerScore;
 return "You lose! " + computerPick + " beats " + userPick;
 }
 }
